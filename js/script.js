1fdc51011b75c3b0f2b2b6f3d82aeb272dffafc2
Dominik Mrówka <dmrowka7@gmail.com>
2022-03-09 13:25:28 +0100

    Add function remove class .active

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 const titleClickHandler = function(event){
-  console.log('Link was clicked!');
-  console.log(event);
+    console.log('Link was clicked!');
+    console.log(event);
   /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts .post');
 
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
@@ -17,5 +26,5 @@ const titleClickHandler = function(event){
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+    link.addEventListener('click', titleClickHandler);
 }
